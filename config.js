328def42f05d32a38ad81aa1124ef97d8b4f9a59
znkf <znkf@myhexin.com>
2019-06-13 17:28:39 +0800

    更新稳定版本文件获取地址方式

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
     VERSION_URI: 'https://code.visualstudio.com/updates/v1_30',
     SHELL_PATH: 'd:/vscode_update_program/update.sh',
     ARCHIVE_PATH: 'd:/vscode_update_program/vscodePackage/vscode.zip',
+    STABLE_URI: 'https://update.code.visualstudio.com/latest/win32-x64-archive/stable',
     MESSAGE: {
         success: 'green', // 成功输出内容
         error: 'red', // 错误输出内容
--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+# curl https://update.code.visualstudio.com/latest/win32-x64-archive/stable >> d:/vscode_update_program/vscodePackage/vscode.zip
 unzip /d/vscode_update_program/vscodePackage/vscode.zip -d /d/vscode_update_program/vscodePackage/package/
 rm -rf /d/vscode/*
 mv /d/vscode_update_program/vscodePackage/package/* /d/vscode/
-rm -rf /d/vscode_update_program/vscodePackage/vscode.zip
+rm -rf /d/vscode_update_program/vscodePackage/vscode.zip /d/vscode_update_program/vscodePackage/package/*
 
 exit
